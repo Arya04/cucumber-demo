f76f0782af7a620d3d5b123f6294534b95e3b31a
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-10-31 16:27:30 -0400

    initial commit

---
+++ src/test/java/com/cucumber/demo/cucumber_demo/TestRunner.java
@@ -0,0 +1,12 @@
+package com.cucumber.demo.cucumber_demo;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features")
+public class TestRunner {
+
+}
